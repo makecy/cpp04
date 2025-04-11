1c1862f13ccc7f5a8a157a6422e34339a34a6031
mstefano <147515168+makecy@users.noreply.github.com>
2025-04-11 22:04:25 +0200

    finished ex02

---
+++ ex02/Animal.cpp
@@ -0,0 +1,33 @@
+#include "Animal.hpp"
+
+Animal::Animal() : type("Animal")
+{
+	std::cout << "Animal constructor called." << std::endl;
+}
+
+Animal::Animal(const Animal &other)
+{
+	*this = other;
+}
+
+Animal &Animal::operator=(const Animal &other)
+{
+	if (this != &other)
+		this->type = other.type;
+	return (*this);
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal destructor called." << std::endl;
+}
+
+std::string Animal::getType() const
+{
+	return (this->type);
+}
+
+void Animal::makeSound() const
+{
+	std::cout << "Animal sound." << std::endl;
+}
---
+++ ex02/main.cpp
@@ -0,0 +1,22 @@
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int	main()
+{
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << "---------------------------------" << std::endl;
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+
+	j->makeSound();
+	i->makeSound();
+
+	delete j;
+	delete i;
+
+	std::cout << "---------------------------------" << std::endl;
+	return (0);
+}
